974dda05886eeac9da1a99f8c3a18bdf59422bbc
CoreOS Admin <core@ip-10-2-31-222.ec2.internal>
2014-08-26 03:26:17 +0000

    Added bin/init.sh

---
+++ bin/init.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+function add_es_master {
+cat >>/elasticsearch/config/elasticsearch.yml <<EEOF
+
+discovery:
+  zen:
+    ping:
+      unicast:
+        ${ES1_PORT_9300_TCP_ADDR}
+EEOF
+}
+
+if [ ! -z "${ES1_PORT_9300_TCP_ADDR}" ] ; then
+  add_es_master
+fi
+
+echo "............"
+echo "Using config file:"
+cat /elasticsearch/config/elasticsearch.yml
+echo "............"
+
+/elasticsearch/bin/elasticsearch
